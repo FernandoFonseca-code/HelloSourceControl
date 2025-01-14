da9c485ee352d7c384960a9b65b4f2e5b5417fe9
FernandoFonseca-code <fernando.fonseca2021@gmail.com>
2025-01-14 11:47:22 -0800

    Added decremented counting output with spacing

--- Hello Source Control/Program.cs
+++ Hello Source Control/Program.cs
@@ -1,7 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, GitHub!");
 
-for (int i = 1; i < 10; i++)
+Console.WriteLine("");
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"Number {i}");
+}
+
+Console.WriteLine("");
+
+Console.WriteLine("Now we will count backwards");
+
+Console.WriteLine("");
+
+for (int i = 10; i >= 1; i--)
 {
     Console.WriteLine($"Number {i}");
 }
